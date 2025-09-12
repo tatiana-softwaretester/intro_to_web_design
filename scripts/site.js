654c688786b7b09fafd7cde71c82e5fdb749ac07
tatianasoloveva <tatiana.softwaretester@gmail.com>
2025-09-12 10:19:07 -0400

    Added Bootstrap

--- scripts/site.js
+++ scripts/site.js
@@ -1,15 +1,27 @@
-function ClearForm(){
-            document.getElementById("contactForm").reset();
-        }
+        function ClearForm(){
+                    document.getElementById("contactForm").reset();
+                }
 
-function hidePTags(){
-        let ptags = document.getElementsByTagName('p');
-        for(let item of ptags){
-            item.style.visibility = 'hidden';
-            item.style.display = 'none';
-            }
-            }
+        function hidePTags(){
+                let ptags = document.getElementsByTagName('p');
+                for(let item of ptags){
+                    item.style.visibility = 'hidden';
+                    item.style.display = 'none';
+                    }
+                    }
 
-$('#jqueryBtn').click(function(){
-$('p').hide();
+        $('#jqueryBtn').click(function(){
+            $('p').hide();
+                    })
+        $(function(){
+            var page = ['index','about','contact'];
+            var pathname = window.location.pathname;
+
+            $('.nav-link').each(function(item){
+                if(pathname.includes(pages[item])){
+                    $(this).addClass('active');
+                    $(this).attr('aria-current', 'page');
+                }
             })
+        })            
+            
